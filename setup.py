901ca15927ef42164762cbaba6287dda98d4eef0
AbigailBuccaneer <AbigailBuccaneer@users.noreply.github.com>
2016-10-28 15:23:14 +0100

    Fix directory layout

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='json2cmake',
-    version='0.1.0',
+    version='0.1.1',
     description='Generate CMakeLists.txt from a compile_commands.json',
     long_description=long_description,
     url='https://github.com/AbigailBuccaneer/json2cmake',
