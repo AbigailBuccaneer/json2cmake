4c7fd020a29029065b1182990e8a9b4ce3122cf0
AbigailBuccaneer <AbigailBuccaneer@users.noreply.github.com>
2018-03-12 21:02:38 +0000

    Bump version to v0.6.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='json2cmake',
-    version='0.6.0',
+    version='0.6.1',
     description='Generate CMakeLists.txt from a compile_commands.json',
     long_description=long_description,
     url='https://github.com/AbigailBuccaneer/json2cmake',
